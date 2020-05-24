76118326164e08c8911a2b6ef5da07258a5b36ff
ManojPathak1 <manoj.pathak1215@gmail.com>
2020-05-24 12:49:06 +0530

    Show hide Quick View, Style changes

--- src/components/Restraunt/Restraunt.js
+++ src/components/Restraunt/Restraunt.js
@@ -1,20 +1,28 @@
-import React, { memo } from "react";
+import React, { memo, useState, useRef } from "react";
+import cx from 'classnames';
 import s from './Restraunt.module.scss';
 import { images } from "../../mockData";
 
 function Restraunt({ restraunt }) {
-  const count = Math.floor(Math.random() * 11);
-  return <div className={s.card}>
-    <img alt="" src={images[count]} height="250px" width="300px" />
+  const [showQuickView, setShowQuickView] = useState(false);
+  const imageSrc = useRef(images[Math.floor(Math.random() * 11)]);
+  return <div onMouseEnter={() => setShowQuickView(true)} onMouseLeave={() => setShowQuickView(false)} className={s.card}>
+    <img alt="" src={imageSrc.current} height="250px" width="300px" />
     <div className={s.details}>
       <span className={s.name}>{restraunt.name}</span>
       <span className={s.foodType}>{restraunt.food_types.join(", ")}</span>
       <div>
-        <span className={s.rating}>&#9733; {restraunt.ratings}</span>
-        <span>-</span>
+        {restraunt.ratings ?
+          <span className={s.rating}>&#9733; {restraunt.ratings}</span>
+          : <span></span>
+        }
+        <span className={s.dot}></span>
         <span>{restraunt.delivery_time}</span>
-        <span>-</span>
+        <span className={s.dot}></span>
         <span>&#8377;{restraunt.price_for_two} FOR TWO</span>
+      </div>
+      <div className={cx({ [s.quickView]: true, [s.quickViewHide]: !showQuickView })}>
+        <span>QUICK VIEW</span>
       </div>
     </div>
   </div>
