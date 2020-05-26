c1b9f50891c9161ac84282bb538288d8cf9f67ef
ManojPathak1 <manoj.pathak1215@gmail.com>
2020-05-26 10:19:28 +0530

    Updates

---
+++ precache-manifest.d0571aa4e4e7bd3850468d086d75a8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cd580e8f39b8272cb2c78c02c6e245",
+    "url": "/react-swiggy/index.html"
+  },
+  {
+    "revision": "c9826fc0f98cb3e87931",
+    "url": "/react-swiggy/static/css/main.3d7c29bb.chunk.css"
+  },
+  {
+    "revision": "904f1b8d0b4bb9fcb751",
+    "url": "/react-swiggy/static/js/2.596e0529.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-swiggy/static/js/2.596e0529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9826fc0f98cb3e87931",
+    "url": "/react-swiggy/static/js/main.2cd3c6ec.chunk.js"
+  },
+  {
+    "revision": "0f745f6b3f9efdfdffa8",
+    "url": "/react-swiggy/static/js/runtime-main.028082cf.js"
+  }
+]);
